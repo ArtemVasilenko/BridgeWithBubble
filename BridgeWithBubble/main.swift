0d215b9eb129d19a94316c7ca403f5cba44810ed
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-18 20:11:24 +0300

    Initial Commit

---
+++ BridgeWithBubble/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BridgeWithBubble
+//
+//  Created by Артем on 7/18/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
